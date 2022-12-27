6780f2ff84b0fab572708136c2d2b7c95de12bc2
Ubaton <raymondNG729@gmail.com>
2022-12-27 21:21:15 +0200

    v-0.1.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      white: "#ffffff",
+      black: "#000000",
+      gray: {
+        75: "#b0b0b0",
+        100: "#808080",
+        200: "#7A7A7A",
+        300: "#747474",
+        400: "#6F6F6F",
+        500: "#696969",
+        600: "#646464",
+        700: "#5E5E5E",
+        800: "#595959",
+        900: "#535353",
+      },
+      ember: "#ff9900",
+    },
     extend: {},
   },
   plugins: [],
